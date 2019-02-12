6d16003eb6d7e71d12c4eaabd7ec0a6f2a3d3350
unknown <TMLocal@TM038-03.Tamweely.Local>
2019-02-12 17:37:15 +0200

    add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './Layout/header/header.component';
+import { FooterComponent } from './Layout/footer/footer.component';
+import { SliderComponent } from './Layout/slider/slider.component';
+import { SidebarComponent } from './Layout/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SliderComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule
